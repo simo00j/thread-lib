6f10248d179d65b0624c53e5d6fafabafb2d1fc9
Ivan “CLOVIS” Canet <ivan.canet@gmail.com>
2021-04-05 10:20:51 +0200

    feat(debug): Non-important messages are logged to stdout, important messages are logged to stderr

--- thread/debug.h
+++ thread/debug.h
@@ -6,7 +6,7 @@
 	#include <stdio.h>
 	#include <stdarg.h>
 
-	#define internal_log(level, message, ...) fprintf(stderr, "[" level "] " message "\n\tin %s:%d, %s\n", __VA_ARGS__, __FILE__, __LINE__, __func__);
+	#define internal_log(file, level, message, ...) fprintf(file, "[" level "] " message "\n\tin %s:%d, %s\n", __VA_ARGS__, __FILE__, __LINE__, __func__);
 
 #else // USE_DEBUG
 
@@ -14,9 +14,9 @@
 
 #endif // USE_DEBUG
 
-#define debug(message, ...) internal_log("DEBUG", message, __VA_ARGS__)
-#define info(message, ...) internal_log("INFO", message, __VA_ARGS__)
-#define warn(message, ...) internal_log("WARN", message, __VA_ARGS__)
-#define error(message, ...) internal_log("ERROR", message, __VA_ARGS__)
+#define debug(message, ...) internal_log(stdout, "DEBUG", message, __VA_ARGS__)
+#define info(message, ...) internal_log(stdout, "INFO", message, __VA_ARGS__)
+#define warn(message, ...) internal_log(stderr, "WARN", message, __VA_ARGS__)
+#define error(message, ...) internal_log(stderr, "ERROR", message, __VA_ARGS__)
 
 #endif //PROJET_D_OS_S8_DEBUG_H
